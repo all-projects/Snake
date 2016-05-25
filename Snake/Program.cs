482d83fa78fcc24c8055af5aedfa19c4ca3a05e1
all-projects <alex1111-test@hotmail.com>
2016-05-24 21:40:00 -0400

    Add List

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -14,7 +14,38 @@ namespace Snake
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();           
+            p2.Draw();
+
+            Point p3 = new Point(2, 2, '*');
+            p3.Draw();
+
+            Point p4 = new Point(3, 2, '*');
+            p4.Draw();
+
+            //-------------------
+
+            //List<int> numList = new List<int>();
+            //numList.Add(0);
+            //numList.Add(1);
+            //numList.Add(2);
+
+            //int x = numList[0];
+            //int y = numList[1];
+            //int z = numList[2];
+
+            //foreach (int i in numList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //numList.Remove(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add( p1 );
+            pList.Add( p2 );
+            pList.Add( p3 );
+            pList.Add( p4 );
+
 
             Console.ReadLine();
         }        
