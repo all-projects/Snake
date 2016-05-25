2094f38848dae5d2cac1137ec757e5f4557fb230
all-projects <alex1111-test@hotmail.com>
2016-05-24 21:13:24 -0400

    C# List class

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,11 +13,8 @@ namespace Snake
             Point p1 = new Point(1, 2, '*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-            p2.Draw(); 
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();           
 
             Console.ReadLine();
         }        
